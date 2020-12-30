a01e3813df38eeac2603f74ace688c0c00ff1f6b
albertoha94 <albertoha94@hotmail.com>
2020-12-30 12:23:27 -0600

    Created camera change code.

---
+++ Assets/TankEngine/Scripts/Utils/CameraTrigger/CameraHandler.cs
@@ -0,0 +1,20 @@
+﻿using Cinemachine;
+using UnityEngine;
+
+public class CameraHandler : MonoBehaviour
+{
+    [SerializeField] CinemachineVirtualCamera InCamera;
+    [SerializeField] CinemachineVirtualCamera OutCamera;
+
+    internal void TriggerCamerasIn()
+    {
+        InCamera.gameObject.SetActive(true);
+        OutCamera.gameObject.SetActive(false);
+    }
+
+    internal void TriggerCamerasOut()
+    {
+        OutCamera.gameObject.SetActive(true);
+        InCamera.gameObject.SetActive(false);
+    }
+}
---
+++ Assets/TankEngine/Scripts/Utils/CameraTrigger/ChildTrigger.cs
@@ -0,0 +1,29 @@
+﻿using TankEngine.Scripts;
+using UnityEngine;
+
+public class ChildTrigger : MonoBehaviour
+{
+
+    [SerializeField] bool isInTrigger;
+    CameraHandler cameraHandler;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (cameraHandler == null)
+        {
+            cameraHandler = transform.parent.GetComponent<CameraHandler>();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != Tags.PLAYER)
+            return;
+
+        if (isInTrigger)
+            cameraHandler.TriggerCamerasIn();
+        else
+            cameraHandler.TriggerCamerasOut();
+    }
+}
