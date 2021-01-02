e4219f80ed6f343f9596a5c2e07dc15f582b5546
albertoha94 <albertoha94@hotmail.com>
2021-01-02 15:48:54 -0600

    Updated scene to handle a broken switch.

---
+++ Assets/Scripts/Props/BrokenLightSwitchInteraction.cs
@@ -0,0 +1,15 @@
+﻿using TankEngine.Scripts.AbstractClasses;
+using TankEngine.Scripts.Interactions;
+using UnityEngine;
+
+public class BrokenLightSwitchInteraction : GameManagerBehavior
+{
+
+    [SerializeField] InteractionBasic interactionToUse;
+
+    public void AddBrokenLightSwitchInteraction()
+    {
+        GameManager.AddInteractionToArray(interactionToUse);
+    }
+
+}
