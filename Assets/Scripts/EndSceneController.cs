80ed7887062de622182e35ffe08ac0f224ee553b
albertoha94 <albertoha94@hotmail.com>
2021-01-11 14:35:26 -0600

    Finished end scene.

---
+++ Assets/Scripts/EndSceneController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndSceneController : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            StartCoroutine(EndGame());
+        }
+    }
+
+    private IEnumerator EndGame()
+    {
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(0);
+    }
+}
